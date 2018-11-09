b8c5e8fee2d80844831fd0055751da355ad70247
Tom Coemans <tom.coemans@gmail.com>
2018-11-09 01:17:50 +0100

    GVR-39 empathy toegepast

--- vodomg-demo.js
+++ vodomg-demo.js
@@ -1,5 +1,5 @@
-import { LitElement, html } from "../../node_modules/vodomg-litelement/@polymer/lit-element/lit-element.js";
-import '../../node_modules/@polymer/iron-component-page.js';
+import { PolymerElement, html } from './assets/@polymer/polymer.js';
+import './assets/@polymer/iron-component-page.js';
 
 class VodomgDemo extends PolymerElement {
     static get is() { return 'vodomg-demo'; }
