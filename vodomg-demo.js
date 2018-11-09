266015d9113576494ce2b37dbc5ef04d41ed05cc
Tom Coemans <tom.coemans@gmail.com>
2018-11-09 01:03:07 +0100

    GVR-39 vodomg-litelement dependency toegevoegd

--- vodomg-demo.js
+++ vodomg-demo.js
@@ -1,4 +1,4 @@
-import { PolymerElement, html } from '../../node_modules/@polymer/polymer.js';
+import { LitElement, html } from "../../node_modules/vodomg-litelement/@polymer/lit-element/lit-element.js";
 import '../../node_modules/@polymer/iron-component-page.js';
 
 class VodomgDemo extends PolymerElement {
